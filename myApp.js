e31728477bab9aafab1b21a8987b4b17e21c27d8
Myo Min Lin <myominlin92@gmail.com>
2025-05-02 11:44:22 +0000

    - changes for NPM section - some changes for Express section

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let app = express();
+//console.log("Hello World");
 
+app.use("/public", express.static(__dirname + "/public"));
 
+const absolutePath = __dirname + "/views/index.html";
 
+app.get("/", (req, res) => {
+    res.sendFile(absolutePath);
+});
 
 
 
